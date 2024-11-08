6d38599707a71ad153918c3d0bc54cb226fbee57
Leosdelrey1111 <hernandezleonel20434@gmail.com>
2024-11-08 09:05:34 -0600

    first commit

--- controllers/APIS/foursquareController.js
+++ controllers/APIS/foursquareController.js
@@ -1,12 +1,12 @@
 const https = require('https');
 
 const foursquareController = {
- buscarLugar: async (req, res) => {
+  buscarLugar: async (req, res) => {
     try {
       const options = {
         method: 'GET',
         hostname: 'api.foursquare.com',
-        path: '/v3/places/search?ll=41.8781,-87.6298&query=restaurante&radius=5000&limit=5',
+        path: '/v3/places/search?ll=21.1561,-100.9319&query=restaurante&radius=5000&limit=50',
         headers: {
           accept: 'application/json',
           Authorization: 'fsq3zsmuEF2kL8pvUzYa06wskpwll/v+kKhij0vB0vS4N54='
@@ -26,13 +26,13 @@ const foursquareController = {
 
           if (data && data.results && data.results.length > 0) {
             for (let place of data.results) {
-                const photos = await getPhotos(place.fsq_id);
-                place.photos = photos; // Agregar las fotos al objeto del lugar
+              const photos = await getPhotos(place.fsq_id);
+              place.photos = photos; // Agregar las fotos al objeto del lugar
             }
             res.status(200).json({
-                success: true,
-                message: 'Lugares encontrados con éxito',
-                data: data.results
+              success: true,
+              message: 'Lugares encontrados en Dolores Hidalgo',
+              data: data.results
             });
           } else {
             res.status(404).json({
@@ -63,50 +63,50 @@ const foursquareController = {
     }
   }
 };
+
 // Función para obtener las fotos de un lugar dado su fsq_id
 const getPhotos = (fsq_id) => {
-    return new Promise((resolve, reject) => {
-      const options = {
-        method: 'GET',
-        hostname: 'api.foursquare.com',
-        path: '/v3/places/${fsq_id}/photos',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'fsq3zsmuEF2kL8pvUzYa06wskpwll/v+kKhij0vB0vS4N54='
-        }
-      };
-  
-      const request = https.request(options, (response) => {
-        const chunks = [];
-  
-        response.on('data', (chunk) => {
-          chunks.push(chunk);
-        });
-  
-        response.on('end', () => {
-          const body = Buffer.concat(chunks).toString();
-          const data = JSON.parse(body);
-  
-          if (data && data.length > 0) {
-            const photos = data.map(photo => {
-              // Obtener la URL de la foto
-              return 'https://fastly.4sqi.net/img/general/${photo.width}x${photo.height}${photo.prefix}${photo.photo_id}${photo.suffix}';
-            });
-            resolve(photos);
-          } else {
-            resolve([]); // No hay fotos disponibles
-          }
-        });
-      });
-  
-      request.on('error', (error) => {
-        console.error('Error en la solicitud a Foursquare para fotos:', error);
-        reject(error);
-      });
-  
-      request.end();
-    });
-  };
-  
+  return new Promise((resolve, reject) => {
+    const options = {
+      method: 'GET',
+      hostname: 'api.foursquare.com',
+      path: `/v3/places/${fsq_id}/photos`,
+      headers: {
+        accept: 'application/json',
+        Authorization: 'fsq3zsmuEF2kL8pvUzYa06wskpwll/v+kKhij0vB0vS4N54='
+      }
+    };
 
-module.exports = foursquareController;  
+    const request = https.request(options, (response) => {
+      const chunks = [];
+
+      response.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+
+      response.on('end', () => {
+        const body = Buffer.concat(chunks).toString();
+        const data = JSON.parse(body);
+
+        if (data && data.length > 0) {
+          // Construimos las URLs de las fotos correctamente
+          const photos = data.map(photo => {
+            return `${photo.prefix}original${photo.suffix}`;
+          });
+          resolve(photos);
+        } else {
+          resolve([]); // No hay fotos disponibles
+        }
+      });
+    });
+
+    request.on('error', (error) => {
+      console.error('Error en la solicitud a Foursquare para fotos:', error);
+      reject(error);
+    });
+
+    request.end();
+  });
+};
+
+module.exports = foursquareController;
